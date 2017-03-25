e6f0a45156023f31c19ba093a5e18a409d747cdc
darrylbutcher <darrylbutcher@hotmail.com>
2017-03-25 16:07:39 +0000

    Made first few forms

--- app/src/main/java/com/example/darryl/bathlaunchpad/MainActivity.java
+++ app/src/main/java/com/example/darryl/bathlaunchpad/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.darryl.bathlaunchpad;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button giveHandBtn;
+    public Button needHandBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        giveHandThread();
+        needHandThread();
     }
+
+    public void giveHandThread(){
+        giveHandBtn=(Button)findViewById(R.id.giveBtn);
+        giveHandBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this,GiveHand.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void needHandThread(){
+        needHandBtn=(Button)findViewById(R.id.needBtn);
+        needHandBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this,NeedHand.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
---
+++ app/src/main/java/com/example/darryl/bathlaunchpad/NeedHand.java
@@ -0,0 +1,13 @@
+package com.example.darryl.bathlaunchpad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NeedHand extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_need_hand);
+    }
+}
