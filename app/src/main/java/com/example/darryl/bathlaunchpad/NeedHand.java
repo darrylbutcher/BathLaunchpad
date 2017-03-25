7e0426f43dc5936d5d185998111cc56989ea4e71
darrylbutcher <darrylbutcher@hotmail.com>
2017-03-25 23:39:01 +0000

    Created Guarding for need hand class

--- app/src/main/java/com/example/darryl/bathlaunchpad/NeedHand.java
+++ app/src/main/java/com/example/darryl/bathlaunchpad/NeedHand.java
@@ -1,14 +1,24 @@
 package com.example.darryl.bathlaunchpad;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class NeedHand extends AppCompatActivity {
 
     public Button submit;
+    private String JobTitle;
+    private String JobRole;
+    private String Description;
+    private String Location;
+    private String Date;
+    private String Hours;
+    private String Pay;
+    private String Skills;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +32,48 @@ public class NeedHand extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent= new Intent(NeedHand.this,ViewApplicants.class);
-                startActivity(intent);
+
+                if(checkInput()){
+                    Toast.makeText(NeedHand.this, "Data Added", Toast.LENGTH_LONG).show();
+                    //addData();
+                    Intent intent= new Intent(NeedHand.this,ViewApplicants.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(NeedHand.this, "ERROR! Make sure you fill in all fields!", Toast.LENGTH_LONG).show();
+                }
             }
         });
+    }
+
+    public boolean checkInput(){
+        setText();
+        if(!JobTitle.equals("")&& !JobRole.equals("")&&!Description.equals("")&&!Location.equals("")&&!Date.equals("")&& !Hours.equals("")&&!Pay.equals("")&&!Skills.equals("")){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void setText(){
+        JobTitle=((EditText)findViewById(R.id.JobTxt)).getText().toString();
+        JobRole=((EditText)findViewById(R.id.roleTxt)).getText().toString();
+        Description=((EditText)findViewById(R.id.descriptionTxt)).getText().toString();
+        Location=((EditText)findViewById(R.id.LocationTxt)).getText().toString();
+        Date=((EditText)findViewById(R.id.dateTxt)).getText().toString();
+        Hours=((EditText)findViewById(R.id.hoursTxt)).getText().toString();
+        Pay=((EditText)findViewById(R.id.payTxt)).getText().toString();
+        Skills=((EditText)findViewById(R.id.skillTxt)).getText().toString();
+    }
+
+    public void clearText(){
+        ((EditText)findViewById(R.id.JobTxt)).setText("");
+        ((EditText)findViewById(R.id.roleTxt)).setText("");
+        ((EditText)findViewById(R.id.descriptionTxt)).setText("");
+        ((EditText)findViewById(R.id.LocationTxt)).setText("");
+        ((EditText)findViewById(R.id.dateTxt)).setText("");
+        ((EditText)findViewById(R.id.hoursTxt)).setText("");
+        ((EditText)findViewById(R.id.payTxt)).setText("");
+        ((EditText)findViewById(R.id.skillTxt)).setText("");
     }
 
 }
